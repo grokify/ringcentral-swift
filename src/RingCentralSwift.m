eea6095c51a7260ccfb645672b61b844716fb3ce
Anil Kumar <anil.akbp@gmail.com>
2015-12-04 15:07:07 -0800

    Adding Bridging-Header

--- src/RingCentralSwift.m
+++
@@ -1,9 +0,0 @@
-//
-//  RingCentralSwift.m
-//  src
-//
-//  Created by Anil Kumar BP on 12/4/15.
-//  Copyright (c) 2015 Anil Kumar BP. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
